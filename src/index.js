ec33632b9f6df6891255cf019652f4fe3fa72d7e
Shamim Reja <shamimad23@gmail.com>
2025-10-14 21:06:30 +0530

    connected to mongodb

---
+++ src/index.js
@@ -0,0 +1,35 @@
+
+import connectDB from "./db/db.js";
+
+connectDB()
+
+
+
+
+
+
+
+
+
+//import mongoose from "mongoose";
+//import { DB_NAME } from "./constants";
+// import express from "express";
+
+// const app = express()
+// IIFE
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error: ", (error) => {
+//             console.log("ERROR: ", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.error("ERROR: ", error)
+//         throw err
+//     }
+// })()
