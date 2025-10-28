24319102673f7257b30613073dc1285d8e67d8c7
Shamim Reja <shamimad23@gmail.com>
2025-10-28 22:00:17 +0530

    dont know

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+import { app } from "./app.js";
 import connectDB from "./db/db.js";
 
 connectDB()
-
+.then(() => {
+    app.listen(process.env.PORT || 8005, () => {
+        console.log("Server is running at port :", process.env.PORT)
+        
+    })
+})
+.catch((err) => {
+    console.log("Mongo db connection failed !!", err);
+    
+})
 
 
 
